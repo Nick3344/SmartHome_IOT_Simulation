9d46af4ee1ead0d784cc0330bbdc4734719d3ea7
Nikhil Gowda <114958232+Nick3344@users.noreply.github.com>
2024-05-05 10:49:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tkinter as tk  
+import tkinter as tk   
 from Devices import SmartLight, Thermostat, SecurityCamera
 from AutomationSystem import AutomationSystem
 from Dashboard import Dashboard
